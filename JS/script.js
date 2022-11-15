160db22d44d68a3cc47b0c832459956d8ee89f1e
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2022-11-15 15:06:29 +0100

    Feat / JS / total sum update when remove item

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,13 @@
 /**
- *  Kund / Varukorgs funktioner, lägga till och tabort varor.
+ *  Kund / Varukorgs funktioner, lägga till och ta bort varor.
+ * 
+ *  1. Lägga till varor i varukorgen
+ *  2. Ta bort varor från varukorgen
+ *  3. Uppdatera totalpriset
+ *  4. Kassa knapp???
  */
 
-// Tabort vara ur kundkorgen.
+// Ta bort vara ur kundkorgen.
 
 const removeProductBtn = document.getElementsByClassName('btn-danger'); // Variabel för att komma åt varje knapp med klassen "btn-danger" (Rensa)
 for (let i = 0; i < removeProductBtn.length; i++) {
@@ -10,5 +15,32 @@ for (let i = 0; i < removeProductBtn.length; i++) {
     removeBtn.addEventListener('click', function(e) {
         let removeBtnClicked = e.target
         removeBtnClicked.parentElement.parentElement.remove();
+        updateTotalPrice();
     })
+
+}
+
+//Uppdatera totalpriset.
+
+
+function updateTotalPrice() {
+    const checkoutCart = document.getElementsByClassName('checkout__cart')[0];
+    const cartRows = checkoutCart.getElementsByClassName('checkout__cart--row');
+    let total = 0;
+
+    for (let i = 0; i < cartRows.length; i++) {
+        const row = cartRows[i];
+        const productPrice = row.getElementsByClassName('cart__product--price')[0];
+        const productQuantity = row.getElementsByClassName('cart__product--amount')[0];
+
+        
+       
+        const price = Number(productPrice.innerText);
+        const quantity = productQuantity.value;
+
+        total = total + (price * quantity);
+        console.log(price);
+    }
+
+    document.getElementById('cart__total__price').innerText = total + ':-';
 }
