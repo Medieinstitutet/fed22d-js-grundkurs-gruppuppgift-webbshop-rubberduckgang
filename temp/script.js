4b636c903fde0d421994bc5bded4a3deaa1cbeab
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2022-11-18 13:12:17 +0100

    test / remade mapstructure

--- temp/script.js
+++ temp/script.js
@@ -74,5 +74,35 @@ function updateTotalPrice() {
 //     }
 // }
 
+/* This is the code for the form, by Hanna*/
 
+/* Toggle between card & invoice*/
+
+const paymentCardInput = document.querySelector('#paymentCard');
+const paymentCardBox = document.querySelector('.hiddenPaymentCard');
+
+const paymentInvoiceInput = document.querySelector("#paymentInvoice");
+const paymentInvoiceBox = document.querySelector(".hiddenPaymentInvoice");
+
+paymentCardInput.addEventListener('click', switchPayment);
+paymentInvoiceInput.addEventListener('click', switchPayment);
+
+function switchPayment(e) {
+
+    if(e.target.id == 'paymentCard') {
+        paymentInvoiceBox.classList.remove('showPaymentInvoice');
+        paymentInvoiceBox.classList.add('hiddenPaymentInvoice');
+
+        paymentCardBox.classList.remove('hiddenPaymentCard');
+        paymentCardBox.classList.add('showPaymentCard');
+    } else {
+        paymentCardBox.classList.remove('showPaymentCard');
+        paymentCardBox.classList.add('hiddenPaymentCard');
+
+        paymentInvoiceBox.classList.remove('hiddenPaymentInvoice');
+        paymentInvoiceBox.classList.add('showPaymentInvoice');
+    }
+}
+
+/* The end of form*/
 
