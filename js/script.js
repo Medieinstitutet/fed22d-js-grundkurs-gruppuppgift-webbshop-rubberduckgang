13f9584e07475572ba51b48d42fc46bc38618cc8
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2022-11-21 16:16:06 +0100

    Fix / js / joel - indentering fixed

--- js/script.js
+++ js/script.js
@@ -80,19 +80,19 @@ function updateTotalPrice() {
 //*****************************************************************************************
 
 function giveMondayDiscount() {
-const mondayDiscount = new Date();
-if ( mondayDiscount.getDay() === 1 &&  mondayDiscount.getHours() < 10) { // söndag = 0, måndag = 1 osv
-    const messageToUser = 'Måndag morgon, varsågod du får 10 % rabatt på din beställning';
-    document.getElementById('msg__to__user').innerText = messageToUser;
+    const mondayDiscount = new Date();
+    if ( mondayDiscount.getDay() === 1 &&  mondayDiscount.getHours() < 10) { // söndag = 0, måndag = 1 osv
+        const messageToUser = 'Måndag morgon, varsågod du får 10 % rabatt på din beställning';
+        document.getElementById('msg__to__user').innerText = messageToUser;
 
-    let reducedPrice = document.getElementById('cart__total__price').innerHTML.replace(':-', '');
+        let reducedPrice = document.getElementById('cart__total__price').innerHTML.replace(':-', '');
   
-    reducedPrice = Number(reducedPrice * 0.9);
-    document.getElementById('cart__total__price').innerHTML = reducedPrice + ':-'
+        reducedPrice = Number(reducedPrice * 0.9);
+        document.getElementById('cart__total__price').innerHTML = reducedPrice + ':-'
 
-} else {
+    } else {
     document.getElementById('msg__to__user').innerText = 'Måndagar före kl 10.00 gäller 10% rabatt'
-}
+    }
 }
 //*****************************************************************************************
 //--------------------------- Manuell rabattkod ------------------------------------------- By J. del Pilar
